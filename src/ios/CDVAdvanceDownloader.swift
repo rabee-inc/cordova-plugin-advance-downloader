048026cad88d465f3d364b69a0700556474ee115
snuffy <snuffy.tokyo@gmail.com>
2020-05-22 10:56:27 +0900

    fix: interface

--- src/ios/CDVAdvanceDownloader.swift
+++ src/ios/CDVAdvanceDownloader.swift
@@ -6,9 +6,20 @@
         // アプリ起動時にここが呼ばれる
         print("hello this is CDVAdvanceDownloader")
     };
+
+    // id と file 名一覧 とステータスが取得できる
+    @objc func getTasks(_ command: CDVInvokedUrlCommand) {}
     
+    // 特定のダウンロード
     @objc func add(_ command: CDVInvokedUrlCommand) {
         // TODO: something
+
+        // parameter として渡したい
+        //{
+        //     url: 'https://hgoehgoe.com'
+        //     path: 'documents/hoehoge',
+        //     fileName: 'hoge.mp4',
+        // }
             
         // js 側で params に渡した第一引数が取れてくる。
         let value = command.argument(at: 0) as! String
@@ -17,7 +28,7 @@
         // let result = CDVPluginResult(status: CDVCommandStatus_OK)
         // result にいろいろなデータを渡して返してあげることができる
          let result = CDVPluginResult(status: CDVCommandStatus_OK,  messageAs: value)
-         
+
         // 辞書式配列で返すことが可能 (その場合勝手に js 側では json になる)
         // let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: ["hoge": "foo"])
         
@@ -29,12 +40,62 @@
         // if progressCallbackId != nil {
         //     let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "10.000")
         //     result?.keepCallback = true
-        //     self.commandDelegate.send(result, callbackId: progressCallbackId)
         // }
 
         // 非同期処理や時間がかかるものに関して commandDelegate の run(inBackground: {}) メソッドを利用してやる
         // commandDelegate.run(inBackground: { 
         //     // 画像アップロード処理とか...
         // })
+    }
+
+
+    // 特定のidを指定してダウンロードをスタートする
+    @objc func start(_ command: CDVInvokedUrlCommand) {
+        let id = command.argument(at: 0) as! String // guard した方がいい？
+        print("start ==========> id: \(id)")
+        let result = CDVPluginResult(status: CDVCommandStatus_OK)
+        commandDelegate.send(result, callbackId: command.callbackId)
+    }
+
+    // 特定のidを指定してダウンロードを中断する
+    @objc func pause(_ command: CDVInvokedUrlCommand) {
+        let id = command.argument(at: 0) as! String // guard した方がいい？
+        print("pause ==========> id: \(id)")
+        let result = CDVPluginResult(status: CDVCommandStatus_OK)
+        commandDelegate.send(result, callbackId: command.callbackId)
+    }
+
+    // 特定のidを指定してダウンロードを再開する
+    @objc func resume(_ command: CDVInvokedUrlCommand) {
+        let id = command.argument(at: 0) as! String // guard した方がいい？
+        print("resume ==========> id: \(id)")
+        let result = CDVPluginResult(status: CDVCommandStatus_OK)
+        commandDelegate.send(result, callbackId: command.callbackId)
+    }
+
+    // 特定のidを指定してダウンロードを注視する
+    @objc func stop(_ command: CDVInvokedUrlCommand) {
+        let id = command.argument(at: 0) as! String // guard した方がいい？
+        print("stop ==========> id: \(id)")
+        let result = CDVPluginResult(status: CDVCommandStatus_OK)
+        commandDelegate.send(result, callbackId: command.callbackId)
+    }
+    
+    // 特定のidのアクションのコールバックの登録
+    // 欲しいイベント [start, pause, resume, stop, complete, fail]
+    @objc func on(_ command: CDVInvokedUrlCommand) {
+        let id = command.argument(at: 0) as! String
+        let event = command.argument(at: 1) as! String
+        let callbackId = command.callbackId
+        // event が callbackId を複数もつイメージ
+        // ここで登録した callbackId を各イベントごとに発火してあげるイメージ
+        // できれば特定の id に対して複数の on ができると嬉しい
+        // { complete: [id1: [callbackId1a, callbackId1b], id2: callbackId2, ...etc ]}
+    }
+
+    // 特定のidのコールバックidを削除する
+    @objc func off(_ command: CDVInvokedUrlCommand) {
+        let id = command.argument(at: 0) as! String
+        let action = command.argument(at: 0) as! String
     }
 }
--- www/AdvanceDownloader.js
+++ www/AdvanceDownloader.js
@@ -2,33 +2,56 @@
 
 var exec = require('cordova/exec');
 
-// cordova exec
-var _AdvanceDownloader = {
-    // param -> path, url, header, filename
-    add: (onSuccess, onFail, param) => {
-        return exec(onSuccess, onFail, 'AdvanceDownloader', 'add', [param]);
-    },
-    // param -> id
-    get: (onSuccess, onFail, param) => {
-
-    },
-
-    
-
-
+// cordova の実行ファイルを登録する
+const registerCordovaExecuter = (action, onSuccess, onFail, param) => {
+    return exec(onSuccess, onFail, 'AdvanceDownloader', action, [param]);
 };
-  
-// Promise wrapper
-var AdvanceDownloader = {
-    add: (params) => {
-        return new Promise((resolve, reject) => {
-            _AdvanceDownloader.add((res) => {
-                resolve(res);
-            }, (err) => {
-                reject(err);
-            }, params);
-        });
+
+// promise で返す。 cordova の excuter の wrapper
+const createAction = (action, params) => {
+    return new Promise((resolve, reject) => {
+        // actionが定義されているかを判定したい
+        if (true) {
+            // cordova 実行ファイルを登録
+            registerCordovaExecuter(action, resolve, reject, params);
+        }
+        else {
+            // TODO: error handling
+        }
+    });
+};
+
+// 本体 -> これを利用したもう一つ大きなクラスを作って 体裁を整える
+const AdvanceDownloader = {
+    // 新規ダウンロードの追加
+    add: (params) => createAction('add', params),
+    // ダウンロードの取得
+    getTasks: (params) => createAction('getTasks', params),
+    // 特定の id のダウンロードの開始
+    start: (params) => createAction('start', params),
+    // 特定の id のダウンロードの一時停止
+    pause: (params) => createAction('pause', params),
+    // 特定の id のダウンロードの再開
+    resume: (params) => createAction('resume', params),
+    // 特定の id のダウンロードの中断
+    stop: (params) => createAction('stop', params),
+    // イベントの登録
+    on: (id, action, callback) => {
+        // action === start | pause | resume | stop | complete | fail;
+        exec(
+            (data) => {
+                // 成功
+                callback(data)
+            },
+            () => {
+                // 失敗
+                // TODO: error handling
+            },'AdvanceDownloader', action, [id, action]
+        );
     },
+    // イベントの削除
+    off: (params) => createAction('off', params),
 }
+
 
 module.exports = AdvanceDownloader;
