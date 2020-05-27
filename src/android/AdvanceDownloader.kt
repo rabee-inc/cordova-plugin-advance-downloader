b1b63fa577be19be6e23f919639711fe18cee959
snuffy <snuffy.tokyo@gmail.com>
2020-05-27 18:54:45 +0900

    fix: for example

--- src/android/AdvanceDownloader.kt
+++ src/android/AdvanceDownloader.kt
@@ -1,12 +1,16 @@
 package jp.rabee
 
 import org.apache.cordova.*
-import org.json.JSONArray
 import org.json.JSONException
 import android.util.Log
+import org.json.*
+
 
 class AdvanceDownloader : CordovaPlugin() {
     lateinit var context: CallbackContext
+
+    // 別の callback context を用意する
+    lateinit var onProgressCallbackContext: CallbackContext
 
     // アプリ起動時に呼ばれる
     override public fun initialize(cordova: CordovaInterface,  webView: CordovaWebView) {
@@ -34,10 +38,20 @@ class AdvanceDownloader : CordovaPlugin() {
 
     // ダウンロードの追加
     private fun add(inputValue: String, callbackContext: CallbackContext): Boolean {
+
         val input = inputValue
         val output = "Kotlin says \"$input\""
-        callbackContext.success(output)
+
+        val result = PluginResult(PluginResult.Status.OK, output)
+        // callback を何回も呼び出したい場合は以下を既述する(ダウンロードの進捗状況を返したい時など)
+        //  result.keepCallback = true
+        callbackContext.sendPluginResult(result)
+
         return true;
+//      時間のかかる処理とか非同期処理は cordova threadPool を使って下しあ
+//        cordova.threadPool.run {
+//            // TODO: 時間のかかる処理
+//        }
     }
 
     companion object {
